5f0f8cefbc2d60c544cc4d07dffa6882b7e92150
Abhik Shah <ashah@lexentbio.com>
2019-07-18 12:00:12 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
 
 setup(
     name='luigi',
-    version='2.8.0.post4rc1',
+    version='2.8.0.post4',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='The Luigi Authors',
